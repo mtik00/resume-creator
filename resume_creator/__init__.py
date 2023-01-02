75137d14369cd8ad54749ffa3c263f3b1e3dccc7
Tim McFadden <mtik00@users.noreply.github.com>
2023-01-02 16:45:18 -0700

    feat: initial pdf resume

--- resume_creator/__init__.py
+++ resume_creator/__init__.py
@@ -4,6 +4,8 @@ import logging
 
 from .settings import settings
 from .markdown import create_markdown
+from .pdf import create_pdf
+
 
 log = logging.getLogger(__name__)
 
@@ -11,6 +13,7 @@ log = logging.getLogger(__name__)
 def main():
     log.debug(settings.data)
     create_markdown()
+    create_pdf()
 
 
 if __name__ == "__main__":
---
+++ resume_creator/pdf.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
+from reportlab.platypus.paragraph import Paragraph
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+
+from .settings import settings
+
+
+def create_pdf():
+    width, height = letter  # keep for later
+
+    fonts_dir = Path(__file__).parent / "fonts"
+    pdfmetrics.registerFont(TTFont("Garamond", fonts_dir / "EBGaramond-Regular.ttf"))
+    pdfmetrics.registerFont(
+        TTFont("Garamond Bold", fonts_dir / "EBGaramond-SemiBold.ttf")
+    )
+    pdfmetrics.registerFont(
+        TTFont("Garamond Medium Italic", fonts_dir / "EBGaramond-Italic.ttf")
+    )
+
+    c = canvas.Canvas("resume.pdf", bottomup=0, pagesize=letter, cropMarks=True)
+
+    y = -0.75 * inch
+    c.translate(0, y)
+    c.setFont("Garamond Bold", 24)
+    c.drawString(0, 0, settings.data["name"])
+
+    c.setFont("Garamond", 16)
+    data = settings.data
+    line = f"{data['email']} \u2022 {data['phone']} \u2022 {data['location']}"
+    y += 1.0 * inch
+    c.drawString(0.05 * inch, y, line)
+
+    y += 0.15 * inch
+    c.setLineWidth(1)
+    c.line(0 * inch, y, width, y)
+
+    y += 0.25 * inch
+    c.setFont("Garamond Bold", 12)
+    c.drawString(0.05 * inch, y, "WORK EXPERIENCE")
+
+    y += 0.15 * inch
+    c.line(0 * inch, y, width, y)
+
+    y = experience(y, c)
+
+    c.save()
+
+
+def experience(y: int, c: canvas.Canvas) -> int:
+    width, height = letter
+    x = 0.05 * inch
+    y += 0.1 * inch
+    for job in settings.data["experiences"]:
+        y += 0.2 * inch
+        c.setFont("Garamond Bold", 12)
+        c.drawString(x, y, job["company"])
+
+        string_width = c.stringWidth(job["dates"], "Garamond Bold", 12)
+
+        c.drawString(width - string_width, y, job["dates"])
+
+        y += 0.2 * inch
+        c.setFont("Garamond Medium Italic", 12)
+        c.drawString(x, y, ", ".join(job["titles"]))
+
+        c.setFont("Garamond", 12)
+        y += 0.2 * inch
+        stylesheet = getSampleStyleSheet()
+        normalStyle = stylesheet["Normal"]
+        style = ParagraphStyle("bullets", normalStyle, bulletIndent=10, leftIndent=20)
+        for item in job["responsibilities"]:
+            p = Paragraph(text=item, style=style, bulletText="\u2022")
+            p.wrapOn(c, width - 0.5 * inch, 0)
+            p.drawOn(c, x, y - p.height + 0.1 * inch)
+            y += p.height
+
+    return y
+
+
+if __name__ == "__main__":
+    create_pdf()
